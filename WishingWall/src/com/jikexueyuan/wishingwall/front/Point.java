6f97a23683eee1f7df0e4e00e661fdce1693f1ff
梦宇@极客学院 <xtiqin@163.com>
2015-06-17 17:42:12 +0800

    提交前端代码

---
+++ WishingWall/src/com/jikexueyuan/wishingwall/front/Point.java
@@ -0,0 +1,30 @@
+package com.jikexueyuan.wishingwall.front;
+
+/**
+ * Created by wwtliu on 6/17/15.
+ */
+public class Point {
+
+    public Point(int x,int y){
+        setX(x);
+        setY(y);
+    }
+
+    int x,y = 0;
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
---
+++ WishingWall/web/WEB-INF/views/addwish.jsp
@@ -0,0 +1,12 @@
+<%@page pageEncoding="UTF-8" %>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <%
+        response.sendRedirect(request.getContextPath());
+    %>
+</body>
+</html>
