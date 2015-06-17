4ff0d33c5e2bdeada661937d9fb22d3a12586648
梦宇@极客学院 <xtiqin@163.com>
2015-06-17 11:44:23 +0800

    修改编码方式Filter的实现方式

--- WishWallPublish/src/com/jikexueyuan/wishingwall/filters/CharsetEncodingFilter.java
+++ WishWallPublish/src/com/jikexueyuan/wishingwall/filters/CharsetEncodingFilter.java
@@ -3,11 +3,13 @@ package com.jikexueyuan.wishingwall.filters;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
 import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
 import java.io.IOException;
 
 /**
  * Created by plter on 6/16/15.
  */
+@WebFilter(filterName = "CharsetFilter",urlPatterns = "/*")
 public class CharsetEncodingFilter extends CharacterEncodingFilter{
 
     public CharsetEncodingFilter(){
